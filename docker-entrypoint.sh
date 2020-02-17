895f461d61a03ae98c7f5f29a7c594c6a4014271
Brian Dwyer <Brian.Dwyer@broadridge.com>
2020-02-17 12:13:51 -0500

    Add more functionality to entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,26 @@
 # Magic to Provision the Container
 # Brian Dwyer - Intelligent Digital Services
 
-tfenv install
+if [ -f '.terraform-version' ]; then
+  echo "Terraform version file detected... Initializing: $(cat .terraform-version)"
+  tfenv install
+fi
 
-# Passthrough
-exec "$@"
+# Kitchen Wrapper & Passthrough
+case "$1" in
+	converge ) kitchen "$*";;
+	create ) kitchen "$*";;
+	destroy ) kitchen "$*";;
+	diagnose | doctor ) kitchen "$*";;
+	exec ) kitchen "$*";;
+	help ) kitchen "$*";;
+	init ) kitchen "$*";;
+	list ) kitchen "$*";;
+	login ) kitchen "$*";;
+	package ) kitchen "$*";;
+	setup ) kitchen "$*";;
+	test ) kitchen "$*";;
+	verify ) kitchen "$*";;
+	version ) kitchen "$*";;
+	* )	exec "$@";;
+esac
